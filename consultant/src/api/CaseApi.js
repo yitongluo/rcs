99413c55ef8f8555db7caa6f66daabdfa4df9486
Yitong Luo <yitong041115@163.com>
2022-08-24 17:50:48 +0800

    Encapsulate Axios into http common

--- consultant/src/api/CaseApi.js
+++ consultant/src/api/CaseApi.js
@@ -1,11 +1,9 @@
 import http from "../http-common";
-import axios from 'axios';
-import authHeader from "./auth-header";
-const API_URL = 'http://localhost:8080/';
+
 
 class CaseApi {
   getAll() {
-    return axios.get(API_URL + "cases", { headers: authHeader() });
+    return http.get("cases");
   }
 
   get(id) {
--- consultant/src/http-common.js
+++ consultant/src/http-common.js
@@ -1,8 +1,43 @@
 import axios from "axios";
+import router from "./router";
 
-export default axios.create({
-  baseURL: "http://localhost:8080/",
-  headers: {
-    "Content-type": "application/json"
-  }
-});
+const baseURL = 'http://localhost:8080'
+const service = axios.create({
+    baseURL: baseURL, // 请求地址前缀，将自动加在 url 前面
+    timeout: 6000 // 请求超时时间
+})
+
+// axios请求拦截器
+service.interceptors.request.use(config => {
+    let user = JSON.parse(localStorage.getItem('user'))
+    if  (user && user.accessToken) {
+        config.headers.Authorization = 'Bearer ' + user.accessToken 
+    }
+    return config
+}, error => {
+    return Promise.reject(error)
+})
+
+// axios响应拦截器
+service.interceptors.response.use(response => {
+    return response.data
+}, error => {
+    // console.log('看一看', error.response)
+    const { status } = error.response
+    if (status === 401) { // token失效
+        console.log('token expired')
+        localStorage.removeItem('user')
+        router.push('/')
+    }
+    return Promise.reject(error)
+})
+
+export default service
+
+
+// export default axios.create({
+//   baseURL: "http://localhost:8080/",
+//   headers: {
+//     "Content-type": "application/json"
+//   }
+// });
